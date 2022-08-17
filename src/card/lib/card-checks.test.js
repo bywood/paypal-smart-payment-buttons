34f1368937b5012316864ddab8ead5a1e896c582
jwplukarski <jwplukarski@gmail.com>
2022-08-17 11:53:23 -0500

    add test for detectCardNumber

--- src/card/lib/card-checks.test.js
+++ src/card/lib/card-checks.test.js
@@ -1,6 +1,13 @@
 /* @flow */
 
-import { checkCardNumber, checkName, checkPostalCode } from "./card-checks";
+import { DEFAULT_CARD_TYPE } from "../constants";
+
+import {
+  checkCardNumber,
+  checkName,
+  checkPostalCode,
+  detectCardType,
+} from "./card-checks";
 
 describe("card-checks", () => {
   describe("checkCardNumber", () => {
@@ -77,6 +84,44 @@ describe("card-checks", () => {
 
       // $FlowFixMe
       expect(checkPostalCode(postalCode).isValid).toBe(false);
+    });
+  });
+
+  describe("detectCardType", () => {
+    it("returns the default card type if the number length is 0", () => {
+      const number = "";
+
+      const cardType = detectCardType(number);
+
+      expect(cardType).toBe(DEFAULT_CARD_TYPE);
+    });
+
+    it("returns the default card type if the number length is greater than 0 but the card validator module does not return any potential card type", () => {
+      // the card-validator module will return an empty array for this card number
+      const number = "123";
+
+      const cardType = detectCardType(number);
+
+      expect(cardType).toBe(DEFAULT_CARD_TYPE);
+    });
+
+    it("returns a card type when the card validator module is able to detect a card type", () => {
+      const number = "411";
+
+      const cardType = detectCardType(number);
+
+      expect(cardType).toStrictEqual({
+        niceType: "Visa",
+        type: "visa",
+        patterns: [4],
+        matchStrength: 1,
+        gaps: [4, 8, 12],
+        lengths: [16, 18, 19],
+        code: {
+          name: "CVV",
+          size: 3,
+        },
+      });
     });
   });
 });
