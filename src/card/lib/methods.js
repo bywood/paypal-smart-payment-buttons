c6290114108d00a17b7a6e0ef482421b2546f6d1
Dean Wood <bywood@paypal.com>
2022-08-29 17:15:56 -0700

    Added rough implementations for the clear and focus methods on CardFields.

--- src/card/lib/methods.js
+++ src/card/lib/methods.js
@@ -29,6 +29,16 @@ export function exportMethods(ref : Object, setAttributes : Function) : void {
         },
         removeClass: (name) => {
             ref?.current?.classList.remove(name);
+        },
+        clear: () => {
+            if (ref && ref.current) {
+                ref.current.value = ''; // This doesn't trigger an onChange event
+                ref.current.dispatchEvent(new Event('change', { 'bubbles': true })); // This didn't help.
+                // Probably need to use setInputState, similar to setAttributes
+            }
+        },
+        focus: () => {
+            ref?.current?.focus();
         }
     });
 };
